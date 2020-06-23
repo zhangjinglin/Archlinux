63066b5c3eb7c083c76469b9c63dc84b8b93266e
Zhang Jinglin <zhangjinglin@gmail.com>
2020-06-23 12:22:56 +0800

    add nvm

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,6 +4,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 cp ~/ArchLinux/config/vimrc ~/.vimrc
 cp ~/ArchLinux/config/zshrc ~/.zshrc
 gem install colorls
