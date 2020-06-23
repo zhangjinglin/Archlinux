5c9daba353a0c4416f89f37ab25456001bda9e14
Zhang Jinglin <zhangjinglin@gmail.com>
2020-06-23 11:51:14 +0800

    modify for wsl-ubuntu

---
+++ setup-ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt install zsh lolcat thefuck ruby-dev neofetch
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ~/ArchLinux/config/vimrc ~/.vimrc
+cp ~/ArchLinux/config/zshrc ~/.zshrc
+gem install colorls
+git config --global user.email "zhangjinglin@gmail.com"
+git config --global user.name "Zhang Jinglin"
