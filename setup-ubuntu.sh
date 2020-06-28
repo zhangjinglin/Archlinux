53f403492288bcfab8813164f0b681928741e619
Zhang Jinglin <zhangjinglin@gmail.com>
2020-06-28 11:19:56 +0800

    modify ubuntu setup

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install zsh lolcat thefuck ruby-dev neofetch
+sudo apt install zsh lolcat thefuck ruby ruby-dev neofetch build-essential
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -6,7 +6,7 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 cp ~/ArchLinux/config/vimrc ~/.vimrc
-cp ~/ArchLinux/config/zshrc ~/.zshrc
-gem install colorls
+cp ~/ArchLinux/config/zshrc-ubuntu ~/.zshrc
+sudo gem install colorls
 git config --global user.email "zhangjinglin@gmail.com"
 git config --global user.name "Zhang Jinglin"
